d2e4d5878329825ea82d9e45e25c99cfd0adf9a6
Hyun Park <hyunp.dev@gmail.com>
2022-11-12 12:21:20 +0900

    update

---
+++ src/test/java/KobisScrapperTest.java
@@ -0,0 +1,15 @@
+import org.cucumber.kobisscrapper.KobisScrapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class KobisScrapperTest {
+    @Test
+    public void getSynopsisByCodeTest() throws IOException {
+        var scrapper = new KobisScrapper(LocalDate.of(2022, 11, 9),
+                                         LocalDate.of(2022, 11, 11));
+        Assertions.assertTrue(scrapper.getSynopsisByCode(20226254).startsWith("“와칸다를 지켜라!” 거대한 두 세계의 충돌,"));
+    }
+}
